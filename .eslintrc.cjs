9900fa0ee9a465c03657370cf671d0a415f37181
luxa4 <luxa4ru@gmail.com>
2024-10-07 15:49:06 +0300

    добавляет eslint конфиг

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,5 +7,25 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  'rules': {
+    indent: ['error', 2], // отступы в файлах JS
+    'vue/script-indent': ['error', 2, { // Отступы в VUE файлах
+      switchCase: 1
+    }],
+    'no-var': ['error'], // заменяет var на let, const
+    'object-curly-spacing': ['error', 'always'], // отступы от curly
+    'comma-dangle': ['error', 'never'], // запрет запятых после пустых элементов
+    'quotes': ['error', 'single'], // замена двойных кавычек на одинарные
+    'semi': ['error', 'always'], // ставит ; в конце строки
+    'max-len': ["error", { "code": 120 }] // максимальное количество кода в строке
+  },
+  'overrides': [
+    {
+      "files": ["*.vue"], // отключаем правило indent, если это Vue файл
+      "rules": {
+        "indent": "off"
+      }
+    }
+  ]
 }
